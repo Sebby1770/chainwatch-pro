06bb3c0cb5e9453c1d54c3972eecd0c2e95c34cf
Sebby1770 <274013474+Sebby1770@users.noreply.github.com>
2026-07-05 13:21:58 +1000

    fix: resolve canvas mock type error in test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,12 +1,26 @@
 import '@testing-library/jest-dom/vitest'
 
-HTMLCanvasElement.prototype.getContext = () =>
-  ({
-    clearRect: () => undefined,
-    beginPath: () => undefined,
-    arc: () => undefined,
-    fill: () => undefined,
-    moveTo: () => undefined,
-    lineTo: () => undefined,
-    stroke: () => undefined,
-  }) as unknown as CanvasRenderingContext2D
+class ResizeObserverMock {
+  observe() {
+    return undefined
+  }
+  unobserve() {
+    return undefined
+  }
+  disconnect() {
+    return undefined
+  }
+}
+
+globalThis.ResizeObserver = ResizeObserverMock as typeof ResizeObserver
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+HTMLCanvasElement.prototype.getContext = ((_contextId: string) => ({
+  clearRect: () => undefined,
+  beginPath: () => undefined,
+  arc: () => undefined,
+  fill: () => undefined,
+  moveTo: () => undefined,
+  lineTo: () => undefined,
+  stroke: () => undefined,
+})) as any
